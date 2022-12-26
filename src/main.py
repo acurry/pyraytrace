f580c6eae697c42b63a93d928399c9b3e209cc53
Andrew Curry <acurry@dragos.com>
2022-12-25 21:48:32 -0500

    Add sphere intersection computation

--- src/main.py
+++ src/main.py
@@ -6,6 +6,31 @@ OUTPUT_DIR = './output'
 width = 300
 height = 200
 
+def normalize(vector: np.ndarray):
+    return vector / np.linalg.norm(vector)
+
+def sphere_intersect(center, radius, ray_origin, ray_direction):
+    b = 2 * np.dot(ray_direction, ray_origin - center)
+    c = np.linalg.norm(ray_origin - center) ** 2 - radius ** 2
+    delta = b ** 2 - 4 * c
+    if delta > 0:
+        t1 = (-b + np.sqrt(delta)) / 2
+        t2 = (-b - np.sqrt(delta)) / 2
+        if t1 > 0 and t2 > 0:
+            return min(t1, t2)
+    return None
+
+
+def nearest_intersected_object(objects, ray_origin, ray_direction):
+    distances = [sphere_intersect(obj['center'], obj['radius'], ray_origin, ray_direction) for obj in objects]
+    nearest_object = None
+    min_distance = np.inf
+    for index, distance in enumerate(distances):
+        if distance and distance < min_distance:
+            min_distance = distance
+            nearest_object = objects[index]
+    return nearest_object, min_distance
+
 def main():
     camera = np.array([0, 0, 1])
     ratio = float(width) / height
@@ -16,11 +41,29 @@ def main():
         'BOTTOM': -1 / ratio,
     }
 
+    objects = [
+        { 'center': np.array([-0.2, 0, -1]), 'radius': 0.7 },
+        { 'center': np.array([0.1, -0.3, 0]), 'radius': 0.1 },
+        { 'center': np.array([-0.3, 0, 0]), 'radius': 0.15 }
+    ]
+
     image = np.zeros((height, width, 3))
     for i, y in enumerate(np.linspace(screen['TOP'], screen['BOTTOM'], height)):
         for j, x in enumerate(np.linspace(screen['LEFT'], screen['RIGHT'], width)):   
-            pass
+            pixel = np.array([x, y, 0])
+            origin = camera
+            direction = normalize(pixel - origin)
+
+            # check for intersections
+            nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
+            if nearest_object is None:
+                continue
+
+            # compute intersection point between ray and nearest object
+            intersection = origin + min_distance * direction
+
             # image[i, j] = ...
+            print("%d/%d" % (i + 1, height))
 
     plt.imsave(f'{OUTPUT_DIR}/image.png', image)
 
