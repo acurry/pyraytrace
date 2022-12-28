5d945821f3df992e9ba7cc6851bf71a432575e2b
Andrew Curry <acurry@dragos.com>
2022-12-27 19:20:15 -0500

    Add yaml loading from a scene.yaml file

--- src/scene.py
+++ src/scene.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-
+from yaml import safe_load
 
 @dataclass
 class Scene:
@@ -9,10 +9,16 @@ class Scene:
     ratio: float = float(width) / height
 
     screen_left: float = -1
-    screen_right: float = 1 / ratio
+    screen_top: float = 1 / ratio
     screen_right: float = 1
     screen_bottom: float = -1 / ratio
-    
+
     camera_x: float = 0.0
     camera_y: float = 0.0
     camera_z: float = 1.0
+
+    @staticmethod
+    def init_from_file(filename: str):
+        with open(filename, "r") as file:
+            data = safe_load(file)
+            return Scene(**data['scene'])
